13475663f80e10c1cb9477d5415e4e35a8aa3ab7
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-10-22 21:35:24 +0200

    gallery done

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -8,12 +8,45 @@ import HeaderImage from '../../assets/images/header_bg_3.jpg';
 import './gallery.css';
 
 function Gallery() {
+  const galleryLength = 15;
+  const images = [];
+  const altText = [
+    'kupine',
+    'maline',
+    'jabuke',
+    'grasak',
+    'kupine',
+    'maline',
+    'jabuke',
+    'grasak',
+    'kupine',
+    'maline',
+    'jabuke',
+    'grasak',
+    'kupine',
+    'maline',
+    'jabuke',
+  ];
+
+  for (let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../assets/images/gallery${i}.jpg`));
+  }
+
   return (
     <>
       <Header title={'Our Gallery'} image={HeaderImage}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
         incidunt voluptatem ut, ipsa libero sunt!
       </Header>
+      <section className="gallery">
+        <div className="container gallery__container">
+          {images.map((image, index) => (
+            <article key={index}>
+              <img src={image} alt={altText[index]} />
+            </article>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
