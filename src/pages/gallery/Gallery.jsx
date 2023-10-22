7b8a52048eea881fe4c47ed4506d3e8a80acbd97
Miroslav Jovic <jovicmiroslav88@gmail.com>
2023-10-22 20:47:36 +0200

    start gallery

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,9 +1,21 @@
-import './gallery.css'
+// components
+import Header from '../../components/header/Header';
+
+// images
+import HeaderImage from '../../assets/images/header_bg_3.jpg';
+
+// style
+import './gallery.css';
 
 function Gallery() {
   return (
-    <div>gallery</div>
-  )
+    <>
+      <Header title={'Our Gallery'} image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        incidunt voluptatem ut, ipsa libero sunt!
+      </Header>
+    </>
+  );
 }
 
-export default Gallery
+export default Gallery;
